4e8af55ce3fd8cff353d1ec3201c5879d5d18f89
정주원 <juwonjeong0123@gmail.com>
2024-11-27 19:00:25 +0900

    테스트

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-나는 빢빢이다!
+노드 설치하세요 게이야
+
+폴더에서 npm init -y 치세요 게이야
+
+npx tsc --init 
+npm i react react-dom
+npm i -D @types/react @types/react-dom
+
+끝이다 이 ㅅㄲ야
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <h2>Home Page</h2>
+      <p>This is the home page of the application.</p>
+    </main>
+  );
+};
+
+export default Home;
